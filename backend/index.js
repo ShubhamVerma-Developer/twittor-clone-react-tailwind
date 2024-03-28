b3e8d92bdca3415765ac9fd9951feb0059e610f2
ShubhamVermaTheDeveloper <shubham25122001@gmail.com>
2024-03-28 13:13:04 +0530

    add the cors policy to allow the localhost 3000 origin

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import databaseConnection from "./config/database.js";
 import cookieParser from "cookie-parser";
 import userRoute from "./routes/userRoute.js";
 import tweetRoute from "./routes/tweetRoute.js";
+import cors from "cors";
 
 dotenv.config({
   path: ".env",
@@ -21,6 +22,11 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 // api
 app.use("/api/v1/user", userRoute);
