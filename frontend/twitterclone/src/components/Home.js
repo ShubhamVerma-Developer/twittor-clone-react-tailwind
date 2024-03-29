a62f975c9847713263203ada6d1dc6d5ec7dff6c
ShubhamVermaTheDeveloper <shubham25122001@gmail.com>
2024-03-29 11:17:53 +0530

    other users follow done

--- frontend/twitterclone/src/components/Home.js
+++ frontend/twitterclone/src/components/Home.js
@@ -2,13 +2,18 @@ import React from "react";
 import LeftSidebar from "./LeftSidebar";
 import RightSidebar from "./RightSidebar";
 import { Outlet } from "react-router-dom";
+import useOtherUsers from "../hooks/useOtherUsers";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const { user, otherUsers } = useSelector((store) => store.user);
+  console.log(user);
+  useOtherUsers(user?._id);
   return (
     <div className="flex justify-between w-[80%] mx-auto">
       <LeftSidebar />
       <Outlet />
-      <RightSidebar />
+      <RightSidebar otherUsers={otherUsers} />
     </div>
   );
 };
--- frontend/twitterclone/src/components/RightSidebar.js
+++ frontend/twitterclone/src/components/RightSidebar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { CiSearch } from "react-icons/ci";
 import Avatar from "react-avatar";
 
-function RightSidebar() {
+function RightSidebar({ otherUsers }) {
   return (
     <div className="w-[25%]">
       <div className="flex text-gray-500 items-center p-2 bg-gray-100 rounded-full outline-none w-full">
@@ -15,67 +15,34 @@ function RightSidebar() {
       </div>
       <div>
         <div className="p-4 bg-gray-100 rounded-2xl my-4">
-          <h1 className="">Who to follow</h1>
-          <div className="flex ic justify-between my-3">
-            <div className="flex">
-              <div>
-                <Avatar
-                  src="https://pbs.twimg.com/profile_images/1703261403237502976/W0SFbJVS_400x400.jpg"
-                  size="40"
-                  round={true}
-                />
+          <h1 className="font-bold text-lg">Who to follow</h1>
+          {otherUsers?.map((user) => {
+            return (
+              <div
+                key={user?._id}
+                className="flex item-center justify-between my-3"
+              >
+                <div className="flex">
+                  <div>
+                    <Avatar
+                      src="https://pbs.twimg.com/profile_images/1703261403237502976/W0SFbJVS_400x400.jpg"
+                      size="40"
+                      round={true}
+                    />
+                  </div>
+                  <div className="ml-2">
+                    <h1 className="font-bold">{user?.name}</h1>
+                    <p className="text-sm">{`${user?.username}`}</p>
+                  </div>
+                  <div>
+                    <button className="px-4 py-1 bg-black text-white rounded-full">
+                      Profile
+                    </button>
+                  </div>
+                </div>
               </div>
-              <div className="ml-2">
-                <h1 className="font-bold">Patel</h1>
-                <p className="text-sm">@patellearnstack</p>
-              </div>
-              <div>
-                <button className="px-4 py-1 bg-black text-white rounded-full">
-                  Profile
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="flex ic justify-between my-3">
-            <div className="flex">
-              <div>
-                <Avatar
-                  src="https://pbs.twimg.com/profile_images/1703261403237502976/W0SFbJVS_400x400.jpg"
-                  size="40"
-                  round={true}
-                />
-              </div>
-              <div className="ml-2">
-                <h1 className="font-bold">Patel</h1>
-                <p className="text-sm">@patellearnstack</p>
-              </div>
-              <div>
-                <button className="px-4 py-1 bg-black text-white rounded-full">
-                  Profile
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="flex ic justify-between my-3">
-            <div className="flex">
-              <div>
-                <Avatar
-                  src="https://pbs.twimg.com/profile_images/1703261403237502976/W0SFbJVS_400x400.jpg"
-                  size="40"
-                  round={true}
-                />
-              </div>
-              <div className="ml-2">
-                <h1 className="font-bold">Patel</h1>
-                <p className="text-sm">@patellearnstack</p>
-              </div>
-              <div>
-                <button className="px-4 py-1 bg-black text-white rounded-full">
-                  Profile
-                </button>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </div>
