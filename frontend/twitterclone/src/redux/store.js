040d1a143ce4a0c84ec132285fbfa97f4ecf98d1
ShubhamVermaTheDeveloper <shubham25122001@gmail.com>
2024-04-02 12:00:06 +0530

    persist redux store

--- frontend/twitterclone/src/redux/store.js
+++ frontend/twitterclone/src/redux/store.js
@@ -1,13 +1,39 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
 import tweetSlice from "./tweetSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  user: userSlice,
+  tweet: tweetSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    // actions
-    user: userSlice,
-    tweet: tweetSlice,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST"],
+      },
+    }),
 });
 
 export default store;
